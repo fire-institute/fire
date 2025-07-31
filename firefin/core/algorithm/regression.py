8d2874b2c19b30c66ef8881efe14503d14e291c3
Kaiwei44 <wkz20031215@gmail.com>
2025-07-31 22:51:31 +0800

    Update regression.py
    Add r2, r2_adj, residuals packaging: r2, r2_adj: DataFrame (if there y is 1 dim in the series)
    residuals: dictionary of DataFrame, T*W*N. If axis=1 or window=None, it is a T*N DataFrame

--- firefin/core/algorithm/regression.py
+++ firefin/core/algorithm/regression.py
@@ -5,6 +5,9 @@
 # @File    : least_square.py
 # @Software: PyCharm
 
+
+# 增加r2, r2_adj, residuals的打包: r2, r2_adj: DataFrame（series如果y只有一个）
+# residuals: dictionary of DataFrame, T*W*N, 如果axis=1或window=None就是T*N的 DataFrame
 import textwrap
 import typing
 
@@ -59,6 +62,25 @@ class RegressionResult:
                 return self.sm_result.params
 
     @property
+    def t_alpha(self):
+        """float or None —> t‑stat of intercept α."""
+        if self.fit_intercept:
+            return self.sm_result.tvalues[0]
+        else:
+            return None
+
+    @property
+    def tvalue(self):
+        """t‑statistics corresponding to the beta coefficients (intercept excluded if `fit_intercept`)."""
+        if self.univariate:
+            return self.sm_result.tvalues[-1]
+        else:
+            if self.fit_intercept:
+                return self.sm_result.tvalues[1:]
+            else:
+                return self.sm_result.tvalues
+
+    @property
     def r2(self):
         """
         Return the coefficient of determination R² of the regression.
@@ -105,6 +127,8 @@ class BatchRegressionResult:
         The regression coefficients.
     alpha: optional
         The intercept term, default is None.
+    tvalue: optional
+        The t‑statistics corresponding to `beta`, default is None.
     r2: optional
         The coefficient of determination R², default is None.
     r2_adj: optional
@@ -116,7 +140,10 @@ class BatchRegressionResult:
     def __init__(
         self,
         beta,
+        *,
         alpha=None,
+        tvalue=None,
+        alpha_t=None,
         r2=None,
         r2_adj=None,
         residuals=None,
@@ -124,6 +151,8 @@ class BatchRegressionResult:
         # NOTE: public names will be displayed in __repr__
         self.alpha = alpha
         self.beta = beta
+        self.tvalue = tvalue
+        self.alpha_t = alpha_t
         self.r2 = r2
         self.r2_adj = r2_adj
         self.residuals = residuals
@@ -171,9 +200,9 @@ def _regression(
     sm.regression.linear_model.RegressionResults
         The regression results.
     """
-    
-    ## if x contains nan, fill nan with 0
-    ## TODO: fill nan with 0 is not a good idea, we should use the mean of the column to fill nan
+
+    # if x contains nan, fill nan with 0
+    # TODO: fill nan with 0 is not a good idea, we should use the mean of the column to fill nan
     x = np.nan_to_num(x, nan=0)
     y = np.nan_to_num(y, nan=0)
 
@@ -182,7 +211,7 @@ def _regression(
     if w is None:
         model = sm.OLS(y, x)
     else:
-        ## TODO: fill nan with 0 is not a good idea, we should use the mean of the column to fill nan
+        # TODO: fill nan with 0 is not a good idea, we should use the mean of the column to fill nan
         y = np.nan_to_num(y, nan=0)
         model = sm.WLS(y, x, weights=w)
 
@@ -240,15 +269,27 @@ def least_square(
 def calculate_window(x_wind, y_wind, w_wind, m1, m2, m3, m, fit_intercept, univariate, cov_type, cov_kwds):
     alphas: list[float] = []
     betas: list[float | np.ndarray | None] = []
+    tvals: list[float | np.ndarray | None] = []
+    talphas: list[float] = []
+    r2s: list[float] = []
+    r2adjs: list[float] = []
+    resmats = []
+
+    win_len = x_wind.shape[1]
+
     for j in range(m):
         x_j = x_wind[:, :, min(j, m1 - 1)].T
         y_j = y_wind[:, min(j, m2 - 1)]
         w_j = None if w_wind is None else w_wind[:, min(j, m3 - 1)]
         # if any x is all nan, skip regression
         if np.isnan(x_j).all(axis=0).any() or np.isnan(y_j).all():
-            # alpha 一定是float；beta可能是array或者float，所以用None表示🈳
             alpha = np.nan
             beta = None
+            tval = None
+            ta = np.nan
+            r2_val = np.nan
+            r2_adj_val = np.nan
+            resid_arr = np.full(win_len, np.nan)
         else:
             res = RegressionResult(
                 # fit_intercept is always False, because we've padded X in __init__
@@ -258,10 +299,23 @@ def calculate_window(x_wind, y_wind, w_wind, m1, m2, m3, m, fit_intercept, univa
             )
             alpha = res.alpha
             beta = res.beta
+            tval = res.tvalue
+            ta = res.t_alpha
+            r2_val = res.r2
+            r2_adj_val = res.r2_adj
+            resid_arr = res.residuals
+
         alphas.append(alpha)
         betas.append(beta)
+        tvals.append(tval)
+        talphas.append(ta)
+        r2s.append(r2_val)
+        r2adjs.append(r2_adj_val)
+        resmats.append(resid_arr)
 
-    return alphas, betas
+    residual_mat = np.column_stack(resmats) #list[1D] → 2D (window × m)
+
+    return alphas, betas, tvals, talphas, r2s, r2adjs, residual_mat
 
 
 class RollingRegressor:
@@ -313,7 +367,7 @@ class RollingRegressor:
         else:
             raise ValueError("parsed x should be array")
 
-        # now x is 3d array: key-index-columns
+            # now x is 3d array: key-index-columns
         if fit_intercept:
             self.x = np.concatenate([np.ones((1, *self.x.shape[1:])), self.x])
 
@@ -503,7 +557,14 @@ class RollingRegressor:
             window = n
 
         alpha = np.full((n, m), np.nan)
+        alpha_t = np.full((n, m), np.nan)
         beta = np.full((k - fit_intercept, n, m), np.nan)
+        tvalue = np.full((k - fit_intercept, n, m), np.nan)
+        r2 = np.full((n, m), np.nan)  # >>> 新增 R²：矩阵
+        r2_adj = np.full((n, m), np.nan)
+        use_dict = (window is not None and axis == 0)  # True → 字典模式
+        residuals_dict = {} if use_dict else None
+        residual_full = None
 
         result_gen = Parallel(n_jobs=n_jobs, verbose=verbose, return_as="generator")(
             calculate_window(
@@ -521,44 +582,82 @@ class RollingRegressor:
             )
             for i in range(n - window + 1)
         )
-        for i, (alphas, betas) in enumerate(result_gen):
+        for i, (alphas, betas, tvals, talphas, r2s, r2adjs, resmat) in enumerate(result_gen):
             alpha[i + window - 1] = alphas
-            for j, _beta in enumerate(betas):
+            alpha_t[i + window - 1] = talphas
+            r2[i + window - 1] = r2s  # >>> 新增 R²
+            r2_adj[i + window - 1] = r2adjs
+            for j, (_beta, _t) in enumerate(zip(betas, tvals)):
                 if _beta is not None:
                     beta[:, i + window - 1, j] = _beta
+                    tvalue[:, i + window - 1, j] = _t
+
+            if use_dict:
+                end_key = self.index[i + window - 1] if self.index is not None else i + window - 1
+                idx_window = self.index[i: i + window] if self.index is not None else range(i, i + window)
+                col_labels = self.columns
+                residuals_dict[end_key] = pd.DataFrame(resmat, index=idx_window, columns=col_labels)
+            else:
+                residual_full = resmat
 
         # squeeze if table
         if is_table:
             # columns
             alpha = alpha[-1]
+            r2 = r2[-1]  # >>> 新增 R²
+            r2_adj = r2_adj[-1]
             # keys x columns
             beta = beta[:, -1]
+            alpha_t = alpha_t[-1]
+            tvalue = tvalue[:, -1]
         # maybe transpose back
         if transpose:
             beta = self._transpose_or_none(beta)
+            tvalue = self._transpose_or_none(tvalue)
         # wrap dataframe if possible
         if is_table:
             alpha = pd.Series(alpha, index=index if transpose else columns, name="alpha")
+            alpha_t = pd.Series(alpha_t, index=index if transpose else columns, name="alpha_t")
+            r2 = pd.Series(r2, index=index if transpose else columns, name="r2")  # >>> 新增 R²
+            r2_adj = pd.Series(r2_adj, index=index if transpose else columns, name="r2_adj")
             if transpose:
                 # axis = 1
                 beta = pd.DataFrame(beta, index=index, columns=keys)
+                tvalue = pd.DataFrame(tvalue, index=index, columns=keys)
             else:
                 beta = pd.DataFrame(beta, index=keys, columns=columns)
+                tvalue = pd.DataFrame(tvalue, index=keys, columns=columns)
             if self.is_univariate:
                 beta = beta.squeeze(axis=axis)
+                tvalue = tvalue.squeeze(axis=axis)
         else:
             alpha = pd.DataFrame(alpha, index=index, columns=columns)
+            alpha_t = pd.DataFrame(alpha_t, index=index, columns=columns)
+            r2 = pd.DataFrame(r2, index=index, columns=columns)  # >>> 新增 R²
+            r2_adj = pd.DataFrame(r2_adj, index=index, columns=columns)
             if self.is_univariate:
                 beta = pd.DataFrame(np.squeeze(beta, axis=0), index=index, columns=columns)
+                tvalue = pd.DataFrame(np.squeeze(tvalue, axis=0), index=index, columns=columns)
             else:
                 beta = [pd.DataFrame(beta[i], index=index, columns=columns) for i in range(k - fit_intercept)]
+                tvalue = [pd.DataFrame(tvalue[i], index=index, columns=columns) for i in range(k - fit_intercept)]
                 if keys is not None:
                     for _key, _beta in zip(keys, beta):
                         _beta.name = _key
-        return BatchRegressionResult(beta, alpha=alpha)
+                    for _key, _tv in zip(keys, tvalue):
+                        _tv.name = _key
+
+        if not use_dict:
+            if transpose:  # axis=1 时 resmat 行/列需要转置回来
+                residual_full = residual_full.T
+            residuals_out = pd.DataFrame(residual_full, index=self.index, columns=self.columns)
+        else:
+            residuals_out = residuals_dict
+
+        return BatchRegressionResult(beta, alpha=alpha, tvalue=tvalue, alpha_t=alpha_t, r2=r2, r2_adj=r2_adj, residuals=residuals_out)
 
 
-def rolling_regression(x, y, window, w=None, *, fit_intercept=True):
+def rolling_regression(x, y, window, w=None, cov_type: str | None = None, *, fit_intercept=True):
     """
     Perform rolling regression.
 
@@ -580,7 +679,7 @@ def rolling_regression(x, y, window, w=None, *, fit_intercept=True):
     BatchRegressionResult
         The batch regression result object.
     """
-    return RollingRegressor(x, y, w, fit_intercept=fit_intercept).fit(window)
+    return RollingRegressor(x, y, w, fit_intercept=fit_intercept).fit(window, cov_type= cov_type)
 
 
 def table_regression(x, y, w=None, *, fit_intercept=True, axis=1):
